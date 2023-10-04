6ae4b9ab25586a0a21494a5cd986a791ce71bec0
j-yesung <hisung-ah@kakao.com>
2023-10-04 19:07:52 +0900

    메인 페이지 레이아웃 (수정 필요)

---
+++ src/js/common.js
@@ -0,0 +1,35 @@
+// 뒤로가기
+function goBack() {
+  window.history.back();
+}
+// 홈으로 이동
+function home() {
+  location.href = '../html/main.html';
+}
+// 상세 페이지 이동
+function goContentsPage() {
+  location.href = '../html/contents.html';
+}
+
+/**
+ * 문서 로드 시 바로 실행되는 즉시 실행 함수입니다.
+ * 중복되는 태그들이 많아서 한번에 일괄 처리 했습니다.
+ */
+(function () {
+  let infomation = {};
+  let weName = ['장예성', '박가연', '이진호', '김지예', '김건우'];
+
+  weName.forEach(function (name) {
+    let contentHtml = `
+      <div class="btn-box">
+        <button class="btn">
+          <div class="bondee-img">
+            <img src="/" width="50" height="50" />
+          </div>
+          <div class="text">${name}</div>
+        </button>
+      </div>
+    `;
+    $('.container').append(contentHtml);
+  });
+})();
