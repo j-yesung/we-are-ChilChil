75f1b02eca3c3f75b716a041c3ab5717433037e3
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-10-06 14:37:46 +0900

    소개페이지 작성
    소개 페이지 js 및 html 수정 및 추가

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,12 @@
 // 로드 시 자기소개 hide -> 버튼 클릭하면 hide/show
-$(document).ready(function () {
+$(document).ready(async function () {
+  // info-area 채우기
+  const $infoArea = $('.info-area');
+
+  const result = await getDataPromise('../data/data.json');
+  const data = result.data;
+  $infoArea.append(data.map(makeInfoItem));
+
   // 말풍선 숨기기
   $('.bubble').hide();
   // 자기 소개 숨기기
@@ -56,3 +63,107 @@ function goContentsPage() {
     $('.container').append(contentHtml);
   });
 })(); */
+// url을 입력으로 데이터 가져오는 함수
+async function getDataPromise(url) {
+  const result = await fetch(url).then((result) => result.json());
+  return result;
+}
+// 소개 페이지 동적 생성
+function makeInfoItem(data) {
+  const { id, name, tmi, mbti, interesting, introduce, img, advantage, blog } = data;
+  const $infoItem = $(`<div class="${id} user info-item"></div>`);
+  const $heading = $(`<h3>안녕하세요.<br/>저는 ${name}입니다.</h3>`);
+  const $introduceContainer = $('<div class="introduce-container"></div>');
+  const $introduce = $(`<p>${introduce}</p>`);
+  const $bubbleSpeechContainer = $('<div class="bubble-speech-container"></div>');
+  const $bubbleLeft = $('<div class="bubble-left"></div>');
+  const $bubbleRight = $('<div class="bubble-right"></div>');
+  const $img = $('<img />');
+
+  const $bubbleMBTI = $('<div class="bubble right bottom"></div>');
+  const $bubbleinteresting1 = $('<div class="bubble right center"></div>');
+  const $bubbleAdvantage = $('<div class="bubble right top"></div>');
+
+  const $bubbleTMI = $('<div class="bubble left bottom"></div>');
+  const $bubbleBlog = $('<div class="bubble left top blog"></div>');
+  const $bubbleinteresting2 = $('<div class="bubble left center"></div>');
+
+  /* ------------ 버블 시작 ------------  */
+  const textInfo = {
+    MBTI: mbti,
+    TMI: tmi,
+    advantage: advantage,
+    blog: blog,
+  };
+  const bubbleObject = {
+    MBTI: $bubbleMBTI,
+    TMI: $bubbleTMI,
+    advantage: $bubbleAdvantage,
+    blog: $bubbleBlog,
+  };
+
+  // 데이터 동적 넣기 (text)
+  Object.keys(textInfo).forEach((label) => {
+    bubbleObject[label].append(makeTextBubble(label, textInfo[label]));
+  });
+  // 데이터 동적 넣기 (list)
+  const interesting1 = interesting.slice(0, interesting.length / 2),
+    interesting2 = interesting.slice(interesting.length / 2);
+
+  [
+    [$bubbleinteresting1, interesting1],
+    [$bubbleinteresting2, interesting2],
+  ].forEach((data) => {
+    const [parent, items] = data;
+
+    parent.append(makeListBubble('관심분야', items));
+  });
+
+  // 버블 left, right에 아이템들 넣기
+  $bubbleLeft.append([$bubbleMBTI, $bubbleinteresting1, $bubbleAdvantage]);
+  $bubbleRight.append([$bubbleTMI, $bubbleinteresting2, $bubbleBlog]);
+  $bubbleSpeechContainer.append([$bubbleLeft, $img, $bubbleRight]);
+
+  /* ------------ 이미지 ------------  */
+  $img.attr('src', img);
+
+  /* ------------ 소개글 ------------  */
+  $introduceContainer.append($introduce);
+
+  $infoItem.append([$heading, $bubbleSpeechContainer, $introduceContainer]);
+  return $infoItem;
+}
+
+// TEXT 세팅(MBTI, TMI, 장점, 블로그 주소) 함수
+function makeTextBubble(label, desc) {
+  const $label = $(`<div class="label">${label}</div>`);
+  const $desc = $(`<div class="desc">${desc}</div>`);
+  const $wrapper = $('<div class="balloon-text"></div>');
+
+  $wrapper.append([$label, $desc]);
+
+  return $wrapper;
+}
+// LIST 세팅 함수
+function makeListBubble(label, listItems) {
+  const $label = $(`<div class="label">${label}</div>`);
+  const $list = $('<ul class="list"></ul>');
+  // list item 내용들 $list에 추가
+  $list.append(
+    listItems.map((item) => {
+      return $(`<li class="list-item">#${item}</li>`);
+    }),
+  );
+
+  const $wrapper = $('<div class="balloon-list"></div>');
+
+  $wrapper.append([$label, $list]);
+
+  return $wrapper;
+}
+
+/* debug 함수 */
+function tab(x) {
+  console.log(x);
+  return x;
+}
