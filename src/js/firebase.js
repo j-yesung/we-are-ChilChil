6b373e94273929b4c9351fa469a7a7fb8e46d8c1
j-yesung <hisung-ah@kakao.com>
2023-10-10 09:23:27 +0900

    방명록 업데이트 이벤트 추가

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -36,7 +36,7 @@ export async function readGuestBooks() {
       let docHtml = `
             <div class="guestbooks-box">${row['text']}
               <button type="button" class="warning-btn" id="delete-btn">삭제</button>
-              <button type="button" class="success-btn" id="delete-btn">수정</button>
+              <button type="button" class="success-btn" id="modify-btn">수정</button>
             </div>
         `;
       $('.guestbooks-container').append(docHtml);
@@ -68,16 +68,30 @@ export async function writeGuestBook(text, pwd) {
  * @param {string} text
  * @returns {object} {msg: 방명록 업데이트 성공 실패 메시지, 비밀번호 미일치 메시지}
  */
-export async function updateGuestBook(id, pwd, text) {
+export async function updateGuestBook(pwd, text) {
+  // guestbooks 데이터 읽어와서 비번 일치하면 해당 id 값 데이터 수정
   try {
-    const docRef = doc(db, 'guestbooks', id);
-    const docSnap = await getDoc(docRef);
-    // 비밀번호가 미일치 할 경우에
-    if (docSnap.data().pwd !== pwd)
-      return {
-        msg: 'invalid-password',
-      };
-    updateDoc(docRef, { text });
+    // const docSnap = await getDoc(docRef);
+    // const docRef = doc(db, 'guestbooks', docSnap.id);
+    // // 비밀번호가 미일치 할 경우에
+    // if (docSnap.data().pwd !== pwd)
+    //   return {
+    //     msg: 'invalid-password',
+    //   };
+    const docs = await getDocs(collection(db, 'guestbooks'));
+    
+    docs.forEach((docSnap) => {
+      let row = docSnap.data();
+      console.log(docSnap.id)
+
+      // 입력한 비밀번호와 DB에 저장되어 있는 비밀번호가 일치하면 삭제
+      if (pwd == row.pwd) {
+        console.log('완료')
+        return updateDoc(doc(db, 'guestbooks', docSnap.id), { text });
+      }
+    });
+
+    // updateDoc(docRef, { text });
     return { msg: 'update-success' };
   } catch {
     return { msg: 'update-fail' };
@@ -92,21 +106,16 @@ export async function updateGuestBook(id, pwd, text) {
 export async function removeGuestBook(pwd) {
   try {
     const docs = await getDocs(collection(db, 'guestbooks'));
-    console.log(pwd, typeof pwd);
 
     docs.forEach((docSnap) => {
       let row = docSnap.data();
-      // 실제데이터 : getDoc(docRef)
-      // id값이 없네요 x
-      // docRef.id o
-      // docData.id -> undefined
 
+      // 입력한 비밀번호와 DB에 저장되어 있는 비밀번호가 일치하면 삭제
       if (pwd == row.pwd) {
         return deleteDoc(doc(db, 'guestbooks', docSnap.id));
       }
     });
 
-    console.log('삭제완료');
     return { msg: 'remove-success' };
   } catch (e) {
     console.error(e);
@@ -140,7 +149,7 @@ export async function isGuestbooksStatus() {
   console.log('방명록 불러오기 완료');
 
   // 방명록을 삭제할 때, 비밀번호를 입력해서 일치하면 삭제합니다.
-  $('.guestbooks-box button').click(function () {
+  $('.guestbooks-box > #delete-btn').click(function () {
     let pwd = prompt('비밀번호', '');
 
     switch (pwd) {
@@ -153,6 +162,31 @@ export async function isGuestbooksStatus() {
         break;
     }
     removeGuestBook(pwd);
-    window.location.reload();
+    // window.location.reload();
+  });
+
+  // 방명록 수정 버튼 이벤트
+  $('.guestbooks-box > #modify-btn').click(function () {
+    
+
+    $('#modalContainer').removeClass('hidden');
+    $('.pwd-data').css('width', '150px');
+    $('#modalContent').css({
+      'width': '300px',
+      'height': '180px'
+    });
+    $('#modalContent').children('.pwd').css('margin-top', '30px');
+    $('#modalContainer > #modalContent').children('.gb').show();
+    $('#modalContainer > #modalContent').children('#update-btn').show();
+    $('#modalContainer > #modalContent').children('#send-btn').hide();
+    $('#modalContainer > #modalContent').children('#update-btn').addClass('success-btn');
+    
+  });
+  $('#update-btn').click(function () {
+    let pwd = $('.pwd-data').val();
+    let text = $('.gb-data').val();
+
+    console.log('비번/텍스트: ', pwd, text);
+    updateGuestBook(pwd, text);
   });
 }
