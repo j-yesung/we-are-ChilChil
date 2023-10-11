646918b64b3a516726156d3f94050ff548d8d53b
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-10-11 19:21:39 +0900

    글자 쌓이는 버그  다시 수정

--- src/js/textChange.js
+++ src/js/textChange.js
@@ -1,49 +1,43 @@
 const secondText = select('.second-text');
 
-// text를 앞에서 부터 붙여 나간다.
-function moveForward(text) {
-  let textIndex = 0;
-  return setInterval(() => {
-    if (textIndex >= text.length) return;
-    secondText.innerText += text[textIndex++];
-  }, 100);
-}
-// 현재 정해진 text를 지워 나간다
-function moveBack() {
-  return setInterval(() => {
-    if (secondText.innerText.length < 0) return;
-    secondText.innerText = secondText.innerText.slice(0, -1);
-  }, 100);
-}
-// 끝이 나면 다음 텍스트에 대해서 해당 기능들을 수행한다
-function changeText() {
-  const texts = ['개발자', '꿈나무', '칠칠맞조'];
-  let curIndex = 0;
-  // 시작 : toFlag == fales, 끝 플래그 : toFlag === true
-  let backTimeout = null;
-  let forwardTimeout = null;
-
-  return () => {
-    forwardTimeout = moveForward(texts[curIndex++]);
-    curIndex %= texts.length;
+function wait(time) {
+  return new Promise((res) =>
     setTimeout(() => {
-      backTimeout = moveBack();
-    }, 1500);
-    setInterval(() => {
-      // 특정 초 안에 아래 동작을 완수한다.
-      if (backTimeout) clearInterval(backTimeout);
-      if (forwardTimeout) clearInterval(forwardTimeout);
-      console.log('here');
-      forwardTimeout = moveForward(texts[curIndex++]);
-      curIndex %= texts.length;
-      setTimeout(() => {
-        backTimeout = moveBack();
-      }, 1500);
-    }, 3500);
+      res();
+    }, time),
+  );
+}
+
+function moveForward(speed, delay) {
+  return (element) => async (text) => {
+    let textIndex = 0;
+    const size = text.length;
+    while (textIndex < size) {
+      element.innerText += text[textIndex++];
+      await wait(speed);
+    }
+    await wait(delay);
+
+    moveBack(speed)(element);
   };
 }
-
-changeText()();
+function moveBack(speed) {
+  return async (element) => {
+    while (element.innerText) {
+      element.innerText = element.innerText.slice(0, -1);
+      await wait(speed);
+    }
+  };
+}
+const texts = ['개발자', '꿈나무', '칠칠맞조'];
+let curIndex = 0;
+const delay1000 = moveForward(100, 1000);
+delay1000(secondText)(texts[curIndex++]);
+curIndex %= texts.length;
+setInterval(() => {
+  delay1000(secondText)(texts[curIndex++]);
+  curIndex %= texts.length;
+}, 3000);
 
 function select(selector) {
   return document.querySelector(selector);
